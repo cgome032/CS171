7c2b81f53b22cc9660d9ce5a54c46a4f104eabd9
Carlos Gomez <cgome032@ucr.edu>
2018-04-04 15:56:56 -0700

    Added function for histogram

--- Assignment1/question0.py
+++ Assignment1/question0.py
@@ -5,23 +5,37 @@ import numpy as np
 
 # Iris information
 irisFileString = "/home/carlos/Documents/CS171/Assignment1/IrisData/iris.data"
-irisHeaders = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
+
+irisHeaders = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label']
+
 irisData = np.genfromtxt(irisFileString, delimiter=',', dtype=None, names=irisHeaders)
-print(irisData)
-
-plt.hist(irisData['sepal_length'], bins=5)
-plt.title("Sepal Length")
-plt.xlabel("Length")
-plt.ylabel("Frequency")
-
-plt.hist(irisData['sepal_width'], bins=5)
-plt.title("Sepal Width")
-plt.xlabel("Width")
-plt.ylabel("Frequency")
-
+print(irisData)   
 
 # Wine information
 wineFileString = "/home/carlos/Documents/CS171/Assignment1/WineData/wine.data"
 
 #wineData = 
+def makeHistograms(binSize, DataFrame, figure):
+    index = 1
+    for name in DataFrame.dtype.names:
+        plt.figure(figure)
+        if (index <= 4):
+            plt.subplot(2,2,index)
+            plt.tight_layout()
+            plt.hist(DataFrame[name], bins=binSize)
+        #plt.title("Histogram title")
+            plt.xlabel(name)
+            plt.ylabel("Frequency")
+        index += 1
+        
+print("Bin size is 5")
+makeHistograms(10,irisData,1)
 
+print("Bin size is 10")
+makeHistograms(10,irisData,2)
+#
+print("Bin size is 50")
+makeHistograms(50, irisData,3)
+
+print("Bin size is 100")
+makeHistograms(100, irisData,4)
