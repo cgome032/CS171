46ec1f81b9861e71f69aea108f622cf3a2ef31d0
Carlos Gomez <cgome032@ucr.edu>
2018-04-08 23:50:23 -0700

    Finished histograms, box-plots, covariance and standard deviation

--- Assignment1/question0.py
+++ Assignment1/question0.py
@@ -2,40 +2,119 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-# Iris information
-irisFileString = "/home/carlos/Documents/CS171/Assignment1/IrisData/iris.data"
+import urllib.request
+
+"""
+Question 0
+"""
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+raw_data = urllib.request.urlopen(url)
+
 
 irisHeaders = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label']
+irisData = np.genfromtxt(raw_data, delimiter=',', dtype=None, names=irisHeaders)
 
-irisData = np.genfromtxt(irisFileString, delimiter=',', dtype=None, names=irisHeaders)
-print(irisData)   
+setosa = irisData[0:50]
+versicolor = irisData[50:100]
+virginica = irisData[100:150]
+
+#print(setosa)
+#print(versicolor)
+#print(virginica)
+
+
+#print(irisData['sepal_length'])
+#print(irisData)   
 
 # Wine information
 wineFileString = "/home/carlos/Documents/CS171/Assignment1/WineData/wine.data"
 
-#wineData = 
-def makeHistograms(binSize, DataFrame, figure):
-    index = 1
-    for name in DataFrame.dtype.names:
-        plt.figure(figure)
-        if (index <= 4):
-            plt.subplot(2,2,index)
-            plt.tight_layout()
-            plt.hist(DataFrame[name], bins=binSize)
-        #plt.title("Histogram title")
-            plt.xlabel(name)
-            plt.ylabel("Frequency")
-        index += 1
-        
-print("Bin size is 5")
-makeHistograms(10,irisData,1)
 
-print("Bin size is 10")
-makeHistograms(10,irisData,2)
-#
-print("Bin size is 50")
-makeHistograms(50, irisData,3)
+"""
+Question 1
 
-print("Bin size is 100")
-makeHistograms(100, irisData,4)
+Implemented a histogram function
+
+
+"""
+
+def histogram(dataArray, binSize, figure, subIndex, title):
+    arrayMin = np.min(dataArray)
+    arrayMax = np.max(dataArray)
+    print("Array minimum: ", arrayMin)
+#    print("Array maximum: ", arrayMax)
+    width = (arrayMax - arrayMin)/binSize
+#    print("Bar width: ", width)
+    
+    # Index to keep track of bar data
+    lowerBound = arrayMin
+    graphBars = []
+    indicies = []
+    
+    
+    for bar in range(binSize):
+        if bar != binSize-1:
+            # Zero bar - binSize-1 bar is inclusive on left side and exclusive on right side
+            frequency = ((lowerBound <= dataArray) & (dataArray < lowerBound+width)).sum()
+            #print("Frequency: ", frequency)
+            graphBars.append(frequency)
+        else:
+            # Middle bars are inclusive on right side and exclusive on left side
+            frequency = ((lowerBound < dataArray) & (dataArray <= lowerBound+width)).sum()
+            #print("Frequency:", frequency)
+            graphBars.append(frequency)
+        indicies.append(lowerBound)
+        lowerBound+=width
+           
+            
+    plt.figure(figure)
+    plt.subplot(2,2,subIndex)
+    plt.bar(indicies, graphBars, width, align='edge')
+    plt.title(title)
+    plt.xticks(np.arange(arrayMin, arrayMax,width))
+    
+histogram(irisData['sepal_length'], 5, 1, 1,"Sepal Length")
+histogram(irisData['sepal_width'], 5, 1, 2, "Sepal Width")
+histogram(irisData['petal_length'], 5, 1, 3, "Petal Length")
+histogram(irisData['petal_width'], 5, 1, 4, "Petal Width")
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(2)
+plt.boxplot(irisData['sepal_length'],vert=False)
+plt.show()
+
+"""
+Question 2 
+"""
+
+def correlation(x,y):
+    return 0
+
+def covariance(x,y):
+    xMean = np.mean(x)
+    yMean = np.mean(y)
+    numerator = 0
+    for i,j in zip(x,y):
+        numerator += (i*j)
+    return (numerator/len(x)) - xMean * yMean
+
+def stdDeviation(a):
+    aMean = np.mean(a)
+    sum = 0
+    for i in a:
+        sum += i**2
+    return math.sqrt((sum / len(a)) - (aMean ** 2))
+
+testA = np.array([30,36,47,50,52,52,56,60,63,70,70,110])
+print("Standard Deviation: ", stdDeviation(testA))
+            
+testX = np.array([6,5,4,3,2])
+testY = np.array([20,10,14,5,5])
+
+print("Covariance: ",covariance(testX,testY))
